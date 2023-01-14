574e677cbea042b5022a1bea1993837a875a9632
AnthonyCrespo <anthony.crespoy@gmail.com>
2023-01-14 00:44:40 -0500

    Deleted weight_decay argument (it already exists) 2

--- mrcnn/model.py
+++ mrcnn/model.py
@@ -2155,7 +2155,7 @@ class MaskRCNN(object):
                                 md5_hash='a268eb855778b3df3c7506639542a6af')
         return weights_path
 
-    def compile(self, learning_rate, momentum,weight_decay):
+    def compile(self, learning_rate, momentum):
         """Gets the model ready for training. Adds losses, regularization, and
         metrics. Then calls the Keras compile() function.
         """
